4e96e84852e662408ada68c67335dedcfc0c8294
Gaspard <gaspard.nacher06@gmail.com>
2026-09-21 09:43:32 -0300

    Rendre le site responsive et retirer l'onglet About

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,7 +20,7 @@
         height: 100vh;
         overflow: auto;
         }
- 
+
         /* Sidebar links */
         .sidebar a {
         display: block;
@@ -29,19 +28,19 @@
         padding: 16px;
         text-decoration: none;
         }
- 
+
         /* Active/current link */
         .sidebar a.active {
         background-color: #04AA6D;
         color: white;
         }
- 
+
         /* Links on mouse-over */
         .sidebar a:hover:not(.active) {
         background-color: #555;
         color: white;
         }
- 
+
         .sidebar form {
             margin: 0;
         }
@@ -60,45 +59,54 @@
             background-color: #555;
             color: white;
         }
- 
+
         /* Page content. The value of the margin-left property should match the value of the sidebar's width property */
         .main-content {
         margin-left: 200px;
         }
- 
+
         /* On screens that are less than 700px wide, make the sidebar into a topbar */
         @media screen and (max-width: 700px) {
         .sidebar {
             width: 100%;
             height: auto;
             position: relative;
+            display: flex;
+            flex-wrap: wrap;
         }
-        .sidebar a {float: left;}
+        .sidebar a,
+        .sidebar span,
+        .sidebar form {
+            flex: 1 1 auto;
+            text-align: center;
+        }
+        .sidebar button.logout-link {
+            text-align: center;
+        }
         .main-content {margin-left: 0;}
         }
- 
-        /* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+
+        /* On screens that are less than 400px, stack the bar vertically instead of horizontally */
         @media screen and (max-width: 400px) {
-        .sidebar a {
-            text-align: center;
-            float: none;
+        .sidebar {
+            flex-direction: column;
         }
         }
     </style>
- 
+
 </head>
 <body>
- 
+
     <!-- The sidebar -->
     <div class="sidebar">
         <a class="active" href="{{ route('boutique') }}">Boutique</a>
         <a href="{{ route('panier') }}">Panier</a>
- 
+
         @guest
             <a href="{{ route('login') }}">Connexion</a>
             <a href="{{ route('register') }}">Inscription</a>
         @endguest
- 
+
         @auth
             <span class="d-block px-3 py-2">{{ Auth::user()->name }}</span>
             <form action="{{ route('logout') }}" method="POST">
@@ -106,10 +114,10 @@
                 <button type="submit" class="logout-link">Déconnexion</button>
             </form>
         @endauth
- 
-        <a href="#about">About</a>
+
+        
     </div>
- 
+
     <!-- Main content area (navbar + page content) -->
     <div class="main-content">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -117,21 +125,20 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
- 
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
- 
+
             <form class="form-inline my-2 my-lg-0" action="{{ route('boutique') }}" method="GET">
             <input class="form-control mr-sm-2" type="search" name="q" value="{{ request('q') }}" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
         </nav>
- 
+
         <div class="container-fluid mt-4">
             @yield('content')
         </div>
     </div>
- 
+
 </body>
 </html>
- 
--- resources/views/panier.blade.php
+++ resources/views/panier.blade.php
@@ -11,41 +11,43 @@
     @if($cartItems->isEmpty())
         <p>Ton panier est vide.</p>
     @else
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Article</th>
-                    <th>Prix unitaire</th>
-                    <th>Quantité</th>
-                    <th>Sous-total</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($cartItems as $item)
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $item->article->title }}</td>
-                        <td>{{ number_format($item->article->price, 2) }} €</td>
-                        <td>
-                            <form action="{{ route('panier.update', $item) }}" method="POST" class="form-inline">
-                                @csrf
-                                @method('PATCH')
-                                <input type="number" name="quantity" value="{{ $item->quantity }}" min="1" class="form-control form-control-sm" style="width: 70px; display:inline-block;">
-                                <button type="submit" class="btn btn-sm btn-outline-secondary ml-2">Mettre à jour</button>
-                            </form>
-                        </td>
-                        <td>{{ number_format($item->quantity * $item->article->price, 2) }} €</td>
-                        <td>
-                            <form action="{{ route('panier.remove', $item) }}" method="POST" onsubmit="return confirm('Retirer cet article ?');">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-outline-danger">Retirer</button>
-                            </form>
-                        </td>
+                        <th>Article</th>
+                        <th>Prix unitaire</th>
+                        <th>Quantité</th>
+                        <th>Sous-total</th>
+                        <th></th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($cartItems as $item)
+                        <tr>
+                            <td>{{ $item->article->title }}</td>
+                            <td>{{ number_format($item->article->price, 2) }} €</td>
+                            <td>
+                                <form action="{{ route('panier.update', $item) }}" method="POST" class="form-inline">
+                                    @csrf
+                                    @method('PATCH')
+                                    <input type="number" name="quantity" value="{{ $item->quantity }}" min="1" class="form-control form-control-sm" style="width: 70px; display:inline-block;">
+                                    <button type="submit" class="btn btn-sm btn-outline-secondary ml-2">Mettre à jour</button>
+                                </form>
+                            </td>
+                            <td>{{ number_format($item->quantity * $item->article->price, 2) }} €</td>
+                            <td>
+                                <form action="{{ route('panier.remove', $item) }}" method="POST" onsubmit="return confirm('Retirer cet article ?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-outline-danger">Retirer</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
         <h4 class="text-right">Total : {{ number_format($total, 2) }} €</h4>
     @endif
